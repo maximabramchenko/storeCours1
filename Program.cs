3b8ed5aa4a440774d5733393b33cb4ab7309a110
max7 <max7@DESKTOP-DQ2AN4C>
2021-12-20 11:27:20 +0200

    fist commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace storeleson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(-1, 200, 50, 100);
+            rectangle.Widht = 30;
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace storeleson
+{
+    class Rectangle
+    {
+        public int left;
+
+        public int top;
+
+        private int width;
+
+        private int height;
+
+
+
+
+        public Rectangle(int left, int top, int width, int height)
+        {
+            this.left = left;
+            this.top = top;
+
+            if (width <= 0)
+                throw new AggregateException(nameof(width));
+
+            if (height <= 0)
+                throw new AggregateException(nameof(height));
+
+            this.width = width;
+            this.height = height;
+
+
+
+
+
+        }
+
+        public int Widht
+        {
+            get { return width; }
+
+            set
+            {
+                if (value > 0)
+                    width = value;
+            
+            
+            
+            }
+
+        }
+        public int Height
+        {
+            get { return height; }
+
+            set 
+            
+            {
+
+                if (value > 0)
+                    height = value;
+
+
+
+
+            
+            
+            
+            }
+        
+        
+        
+        }
+
+
+
+    }
+}
